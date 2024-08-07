84fa9bce375a9c8222d4bd07e162cfa954f448f7
Samuel Tuoyo <150522328+samueltuoyo@users.noreply.github.com>
2024-08-07 16:01:59 +0100

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-const quoteElement = document.getElementById("quoteElement")
-const apiUrl = `https://api.quotable.io/random`
-function Gen(){
-    fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => quoteElement.textContent = data.content)
-    .catch(error => console.log('error fetching quote!' + error))
-}
